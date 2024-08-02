2335ffae48525fa73ca57b1c222610a2d2c03de4
Pawani Maheshika Bandara <Pawanimaheshika2003@gmail.com>
2024-08-02 21:31:47 +0530

    engine-managent code without testing

--- src/config/routes/trainRoutes.js
+++ src/config/routes/trainRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const trainController = require('../controllers/trainController');
+const protect = require('../middleware/authMiddleware');
 
-router.post('/:id/locations', trainController.receiveGPSData);
-router.get('/', trainController.fetchTrainData);
-router.get('/:id', trainController.fetchSpecificTrainData);
-router.get('/:id/locations', trainController.fetchTrainLocationHistory);
-router.post('/:id/change-engine', trainController.changeEngine);
+router.post('/:id/gps', protect, trainController.receiveGPSData);
+router.get('/', protect, trainController.fetchTrainData);
+router.get('/:id', protect, trainController.fetchSpecificTrainData);
+router.get('/:id/history', protect, trainController.fetchTrainLocationHistory);
+router.post('/:id/change-engine', protect, trainController.changeEngine);
 
 module.exports = router;
