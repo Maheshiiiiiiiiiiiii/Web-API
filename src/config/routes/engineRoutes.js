ec2faa151604b6ae740171d9f8df5b5f82399808
maheshiiiiiiiiiiiii <pawanimaheshika2003@gmail.com>
2024-08-26 02:12:16 +0530

    fix issue

--- src/config/routes/engineRoutes.js
+++ src/config/routes/engineRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { handleEngineChange } = require('../controllers/engineController');
+const engineController = require('../controllers/engineController');
+//const { verifyToken } = require('../utils/verifyToken');
+const { addEngine, removeEngine, getEngines,changeEngine} = require('../controllers/engineController');
+
 
 router.post('/change', async (req, res) => {
   const { train_id, newEngine } = req.body;
@@ -11,29 +14,17 @@ router.post('/change', async (req, res) => {
     res.status(500).json({ message: 'Error changing engine', error });
   }
 });
-const engineController = require('../controllers/engineController');
-const { verifyToken } = require('../utils/verifyToken');
-const { addEngine, removeEngine, getEngines } = require('../controllers/engineController');
-const engineController = require('../controllers/engineController');
-const verifyToken = require('../utils/verifyToken');
-
-const { getEngines, changeEngine } = require('../controllers/engineController');
 
 router.post('/', engineController.addEngine);
 router.get('/', engineController.getEngines);
 router.put('/:id', engineController.updateEngine);
 router.delete('/:id', engineController.deleteEngine);
-router.post('/', verifyToken, addEngine);
-router.delete('/:engine_id', verifyToken, removeEngine);
-router.get('/', verifyToken, getEngines);
-
-router.post('/', verifyToken, engineController.createEngine);
-router.get('/', verifyToken, engineController.getAllEngines);
-router.get('/:id', verifyToken, engineController.getEngineById);
-router.put('/:id', verifyToken, engineController.updateEngine);
-router.delete('/:id', verifyToken, engineController.deleteEngine);
-
+router.post('/', addEngine);
+router.delete('/:engine_id', removeEngine);
 router.get('/', getEngines);
+
+router.post('/', engineController.createEngine);
+router.get('/:id', engineController.getEngineById);
 router.post('/change', changeEngine);
 
 module.exports = router;
--- src/config/routes/healthCheckRoutees.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { healthCheck } = require('../controllers/healthCheckController');
-
-const router = express.Router();
-
-// Health Check Endpoint
-router.get('/health', healthCheck);
-
-module.exports = router;
