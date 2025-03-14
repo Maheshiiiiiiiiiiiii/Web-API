97dcd561f908b46a746494914f2d72291d6cf869
maheshiiiiiiiiiiiii <pawanimaheshika2003@gmail.com>
2025-03-14 13:32:28 +0530

    fix the user registration

--- src/config/routes/authRoutes.js
+++ src/config/routes/authRoutes.js
@@ -4,11 +4,102 @@ const authController = require('../controllers/authController'); // Ensure this 
 const verifyToken = require('../middleware/verifyToken'); // Import the verifyToken middleware
 
 // Public routes
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Successful login
+ *       401:
+ *         description: Unauthorized
+ */
 router.post('/login', authController.loginClient);
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ */
 router.post('/register', authController.registerClient);
 
 // Protected routes
-router.get('/profile', verifyToken, authController.getProfile);// getProfile function is not working that middle ware part is useless 
+/**
+ * @swagger
+ * /auth/profile:
+ *   get:
+ *     summary: Get user profile
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Successful retrieval of user profile
+ *       401:
+ *         description: Unauthorized
+ */
+router.get('/profile', verifyToken, authController.getProfile);
+
+/**
+ * @swagger
+ * /auth/update:
+ *   put:
+ *     summary: Update user profile
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User profile updated successfully
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ */
 router.put('/update', verifyToken, authController.updateProfile);
 
 module.exports = router;
